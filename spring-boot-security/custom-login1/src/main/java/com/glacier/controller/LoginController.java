c4fa489b815a700db1d976f38e2add80fe25cecc
glacier <glacier0315@126.com>
2020-08-18 13:43:01 +0800

    整合spring-boot-security和thymeleaf

---
+++ spring-boot-security/custom-login1/src/main/java/com/glacier/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.glacier.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @date 2020-07-27 12:32
+ */
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
