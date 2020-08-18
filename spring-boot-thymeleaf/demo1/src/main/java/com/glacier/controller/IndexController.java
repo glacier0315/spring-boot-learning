11444cc2376985444a21fe1857bb0b02618b12c4
glacier <glacier0315@126.com>
2020-08-18 13:38:33 +0800

    整合spring-boot-thymeleaf

---
+++ spring-boot-thymeleaf/demo1/src/main/java/com/glacier/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.glacier.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author glacier
+ * @version v1.0.0
+ * @since <pre>2017/4/15 0015</pre>
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        String[] s = {"我是1", "我是2", "我是3", "我是5"};
+        model.addAttribute("s", s);
+        return "core/hello";
+    }
+}
