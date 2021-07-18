1a95b0e2102190d6b7baebfeff7221c0b9c58a49
glacier <glacier0315@126.com>
2021-07-18 15:45:22 +0800

    修改代码

--- spring-boot-mybatis-plus/dynamic1/src/main/java/com/glacier/config/MybatisConfig.java
+++ spring-boot-mybatis-plus/dynamic1/src/main/java/com/glacier/config/MybatisConfig.java
@@ -1,8 +1,6 @@
 package com.glacier.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -18,13 +16,4 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.glacier.mapper")
 public class MybatisConfig {
 
-    /**
-     * mybatis-plus 分页插件
-     *
-     * @return
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
-    }
 }
