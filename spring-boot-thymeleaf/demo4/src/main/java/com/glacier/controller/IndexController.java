abbe835bf1dac425209dda1c3f5eef54f3923052
glacier <glacier0315@126.com>
2021-09-18 16:18:26 +0800

    集成Pear-Admin-Layui

---
+++ spring-boot-thymeleaf/demo4/src/main/java/com/glacier/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.glacier.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author glacier
+ * @version v1.0.0
+ * @since <pre>2017/4/15 0015</pre>
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("")
+    public String index(Model model) {
+        return "index";
+    }
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+}
