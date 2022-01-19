b79d5c4b3bbd5361f4fbead85921334c3c2f7b14
glacier <glacier0315@126.com>
2022-01-19 14:31:22 +0800

    修改spring boot scheduled示例

--- spring-boot-scheduled/demo1/src/main/java/com/glacier/scheduled/ScheduledDemo1Application.java
+++ spring-boot-scheduled/demo1/src/main/java/com/glacier/scheduled/ScheduledDemo1Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
+ * EnableScheduling 启动定时调度器，调度器为单线程
+ *
  * @author glacier
  * @version 1.0
  * @date 2021-04-20 17:30
  */
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class ScheduledDemo1Application {
 
     public static void main(String[] args) {
--- spring-boot-scheduled/demo1/src/main/java/com/glacier/scheduled/job/JobDemo.java
+++ spring-boot-scheduled/demo1/src/main/java/com/glacier/scheduled/job/JobDemo.java
@@ -1,6 +1,7 @@
 package com.glacier.scheduled.job;
 
-import org.springframework.scheduling.annotation.Async;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +13,24 @@ import java.time.LocalDateTime;
  * @date 2021-04-20 17:32
  */
 @Component
-@Async
 public class JobDemo {
-
-    @Scheduled(cron = "0/2 * * * * ?")
-    public void print() {
-        System.out.println("定时任务启动， 当前时间是：\t" + LocalDateTime.now());
-    }
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JobDemo.class);
+	
+	@Scheduled(cron = "0/10 * * * * ?")
+	public void print() {
+		LOGGER.info("print 定时任务启动， 当前时间是：\t" + LocalDateTime.now());
+	}
+	
+	
+	@Scheduled(cron = "0/10 * * * * ?")
+	public void print2() {
+		LOGGER.info("print2 定时任务启动， 当前时间是：\t" + LocalDateTime.now());
+		try {
+			Thread.sleep(15 * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info("print2 定时任务结束， 当前时间是：\t" + LocalDateTime.now());
+	}
 }
