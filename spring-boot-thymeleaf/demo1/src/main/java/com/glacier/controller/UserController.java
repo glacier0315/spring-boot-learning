3c45cd9e5d3e9bcbd17f98875a1f45db2a6894ce
glacier <glacier0315@126.com>
2022-09-21 17:20:46 +0800

    修改thymeleaf 异步刷新

--- spring-boot-thymeleaf/demo1/src/main/java/com/glacier/controller/UserController.java
+++ spring-boot-thymeleaf/demo1/src/main/java/com/glacier/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,21 +20,36 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
-    @GetMapping()
-    public String index(Model model) {
-        log.info("测试thymeleay");
-        List<User> list = new ArrayList<>();
-        User user = null;
-        for (int i = 0; i < 50; i++) {
-            user = new User("id_" + i, "name_" + i, "pw_" + i, i % 2);
-            list.add(user);
-        }
-        model.addAttribute("users", list);
-        return "user/list";
-    }
-
-
+	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@GetMapping()
+	public String index(Model model) {
+		log.info("测试thymeleay");
+		List<User> list = new ArrayList<>();
+		User user = null;
+		for (int i = 0, n = 10; i < n; i++) {
+			user = new User("id_" + i, "name_" + i, "pw_" + i, i % 2);
+			list.add(user);
+		}
+		model.addAttribute("users", list);
+		return "user/list";
+	}
+	
+	@RequestMapping(value = "list")
+	public String list(Model model, User user) {
+		log.info("测试thymeleay {}", user);
+		List<User> list = new ArrayList<>();
+		User item = null;
+		SecureRandom random = new SecureRandom();
+		int num = random.nextInt(100);
+		for (int i = num, n = num + 10; i < n; i++) {
+			item = new User("id_" + i, "name_" + i, "pw_" + i, i % 2);
+			list.add(item);
+		}
+		model.addAttribute("users", list);
+		return "user/list::table_refresh";
+	}
+	
+	
 }
