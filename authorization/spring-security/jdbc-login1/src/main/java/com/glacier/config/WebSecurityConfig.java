f7165ab747046925e31d88defc180ea2f0ac98ea
glacier <glacier0315@126.com>
2022-11-17 16:39:52 +0800

    修改security 示例

--- authorization/spring-security/jdbc-login1/src/main/java/com/glacier/config/WebSecurityConfig.java
+++ authorization/spring-security/jdbc-login1/src/main/java/com/glacier/config/WebSecurityConfig.java
@@ -1,20 +1,16 @@
 package com.glacier.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * security配置
@@ -26,9 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
+public class WebSecurityConfig {
 	
 	/**
 	 * 密码工具类
@@ -41,52 +35,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	/**
-	 * 授权中心管理器，解决依赖注入问题
+	 * 获取AuthenticationManager（认证管理器），登录时认证使用
 	 *
+	 * @param authenticationConfiguration
 	 * @return
 	 * @throws Exception
 	 */
-	@Bean(BeanIds.AUTHENTICATION_MANAGER)
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService)
-				.passwordEncoder(passwordEncoder());
-	}
-	
-	/**
-	 * 配置静态资源拦截问题
-	 *
-	 * @param web
-	 * @throws Exception
-	 */
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-				.antMatchers("/favicon.ico",
-						"/error",
-						"/static/**",
-						"/webjars/**",
-						"/css/**",
-						"/js/**",
-						"/fonts/**");
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeRequests(authorizeRequests -> {
-					authorizeRequests.anyRequest()
-							.authenticated();
-				})
+	@Bean
+	SecurityFilterChain web(HttpSecurity http) throws Exception {
+		http
+				.cors(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests((authorize) ->
+						authorize.mvcMatchers("/favicon.ico",
+										"/error",
+										"/static/**",
+										"/webjars/**",
+										"/css/**",
+										"/js/**",
+										"/fonts/**"
+								)
+								.permitAll()
+								.anyRequest()
+								.authenticated()
+				)
 				.formLogin(formLogin -> {
 					formLogin.loginPage("/login")
 							.permitAll();
 				})
-				.httpBasic();
+				.logout(logout -> {
+					logout.invalidateHttpSession(true)
+							.clearAuthentication(true)
+							.deleteCookies("JSESSIONID")
+							.logoutSuccessUrl("/login");
+				});
+		
+		return http.build();
 	}
 }
