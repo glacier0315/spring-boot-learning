5f251869501e63f74e3c871fa6c71db52bf20d7a
glacier <glacier0315@126.com>
2022-12-30 21:15:31 +0800

    Pear-Admin-Layui: 初始化

---
+++ template-engine/thymeleaf/Pear-Admin-Layui/README.md
@@ -0,0 +1,3 @@
+# Pear-Admin-Layui
+## 说明
+基于 [Pear Admin Layui](https://gitee.com/pear-admin/Pear-Admin-Layui) 3.12.0
---
+++ template-engine/thymeleaf/Pear-Admin-Layui/src/main/java/com/glacier/ThymeleafPearAdminLayuiApplicatin.java
@@ -0,0 +1,18 @@
+package com.glacier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * date 2022-12-30 21:07
+ *
+ * @author glacier
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class ThymeleafPearAdminLayuiApplicatin {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafPearAdminLayuiApplicatin.class, args);
+    }
+}
