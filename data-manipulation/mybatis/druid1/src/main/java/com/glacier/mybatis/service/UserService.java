b74b9086b44919d436bed1b36ba2929ac9330dea
glacier <glacier0315@126.com>
2023-11-25 10:47:15 +0800

    添加编程式事务

--- data-manipulation/mybatis/druid1/src/main/java/com/glacier/mybatis/service/UserService.java
+++ data-manipulation/mybatis/druid1/src/main/java/com/glacier/mybatis/service/UserService.java
@@ -11,6 +11,20 @@ import java.util.List;
  * @version 1.0
  */
 public interface UserService {
+
+    /**
+     * 新增用户
+     * @param user
+     * @return
+     */
+    int add(User user);
+
+    /**
+     *  更新用户
+     * @param user
+     * @return
+     */
+    int update(User user);
 	/**
 	 * 查询所有
 	 *
--- data-manipulation/mybatis/druid1/src/main/java/com/glacier/mybatis/service/impl/UserServiceImpl.java
+++ data-manipulation/mybatis/druid1/src/main/java/com/glacier/mybatis/service/impl/UserServiceImpl.java
@@ -3,10 +3,13 @@ package com.glacier.mybatis.service.impl;
 import com.glacier.mybatis.entity.User;
 import com.glacier.mybatis.mapper.UserMapper;
 import com.glacier.mybatis.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * date 2022-01-21 08:09
@@ -16,20 +19,55 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl implements UserService {
-	private final UserMapper userMapper;
-	
-	@Autowired
-	public UserServiceImpl(UserMapper userMapper) {
-		this.userMapper = userMapper;
-	}
-	
-	/**
-	 * 查询所有
-	 *
-	 * @return 所有实例
-	 */
-	@Override
-	public List<User> selectAll() {
-		return userMapper.selectAll();
-	}
+
+    private TransactionTemplate transactionTemplate;
+    private UserMapper userMapper;
+
+    @Resource
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Resource
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    @Override
+    public int add(User user) {
+        // 其他逻辑
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //  保存
+        Integer add = transactionTemplate.execute(status -> {
+            // 其他逻辑
+            return userMapper.insert(user);
+        });
+        // 其他逻辑
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.ofNullable(add).orElse(0);
+    }
+
+    @Transactional(rollbackFor = {Exception.class})
+    @Override
+    public int update(User user) {
+        return userMapper.updateByPrimaryKey(user);
+    }
+
+    /**
+     * 查询所有
+     *
+     * @return 所有实例
+     */
+    @Override
+    public List<User> selectAll() {
+        return userMapper.selectAll();
+    }
 }
