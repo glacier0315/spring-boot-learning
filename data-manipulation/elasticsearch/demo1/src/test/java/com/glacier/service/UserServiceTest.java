c3e52e4401343458f6d43e8398228486782b8ebd
glacier <glacier0315@126.com>
2023-12-07 22:14:22 +0800

    修改配置

--- data-manipulation/elasticsearch/demo1/src/test/java/com/glacier/service/UserServiceTest.java
+++ data-manipulation/elasticsearch/demo1/src/test/java/com/glacier/service/UserServiceTest.java
@@ -1,13 +1,12 @@
 package com.glacier.service;
 
 import com.glacier.domain.User;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 /**
  * date 2022-01-19 10:02
@@ -17,36 +16,33 @@ import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
  */
 @SpringBootTest
 class UserServiceTest {
-	@Autowired
-	private ElasticsearchRestTemplate elasticsearchRestTemplate;
-	@Autowired
+	@Resource
 	private UserService userService;
-	
+
 	@BeforeEach
 	void setUp() {
 	}
-	
+
 	@AfterEach
 	void tearDown() {
 	}
-	
+
 	@Test
 	void findByUsername() {
-		System.out.println(elasticsearchRestTemplate);
 		String username = "user_1";
 		User user = userService.findByUsername(username);
 		System.out.println(user);
 		Assertions.assertNotNull(user);
 		Assertions.assertEquals(user.getUsername(), username, "查询不正确");
-		
+
 	}
-	
+
 	@Test
 	void findAll() {
 		userService.findAll()
 				.forEach(System.out::println);
 	}
-	
+
 	@Test
 	void findList() {
 		userService.findList(new User())
