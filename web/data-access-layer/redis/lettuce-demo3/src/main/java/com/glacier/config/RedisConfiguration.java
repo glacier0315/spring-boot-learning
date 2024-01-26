e19ce89c07db747380527afb4e7d8db3a76f1eee
glacier <glacier0315@126.com>
2024-01-26 22:26:18 +0800

    perf: 优化redis 代码

--- web/data-access-layer/redis/lettuce-demo3/src/main/java/com/glacier/config/RedisConfiguration.java
+++ web/data-access-layer/redis/lettuce-demo3/src/main/java/com/glacier/config/RedisConfiguration.java
@@ -1,17 +1,14 @@
 package com.glacier.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.glacier.cache.enhance.EnhancedCacheManager;
 import com.glacier.enums.CacheConfigEnums;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.CacheManager;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -62,37 +59,32 @@ public class RedisConfiguration {
 
     /**
      * 配置缓存管理器
-     *
-     * @param redisConnectionFactory Redis 线程安全连接工厂
-     * @return 缓存管理器
+     * @return
      */
     @Bean
-    @Primary
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisSerializer<String> keySerializer = RedisSerializer.string();
-        RedisSerializer<Object> valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+    RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return builder -> {
+            RedisSerializer<String> keySerializer = RedisSerializer.string();
+            RedisSerializer<Object> valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
-        //配置 key value 序列化器，过期时间
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofSeconds(30))
-                .prefixCacheNameWith(CacheConfigEnums.COMMON_CACHE_KEY)
-                .serializeKeysWith((RedisSerializationContext.SerializationPair.fromSerializer(keySerializer)))
-                .serializeValuesWith((RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer)))
-                .disableCachingNullValues();
+            //配置 key value 序列化器，过期时间
+            RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofSeconds(30))
+                    .prefixCacheNameWith(CacheConfigEnums.COMMON_CACHE_KEY)
+                    .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
+                    .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))
+                    .disableCachingNullValues();
 
-        Map<String, RedisCacheConfiguration> cacheMap = Arrays.stream(CacheConfigEnums.values())
-                .collect(Collectors.toMap(CacheConfigEnums::getCacheName,
-                        t -> RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(t.getTtl())
-                                .prefixCacheNameWith(CacheConfigEnums.COMMON_CACHE_KEY)
-                                .serializeKeysWith((RedisSerializationContext.SerializationPair.fromSerializer(keySerializer)))
-                                .serializeValuesWith((RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer)))));
-
-        RedisCacheManager cacheManager = RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(config)
-                .withInitialCacheConfigurations(cacheMap)
-                .transactionAware()
-                .build();
-        return new EnhancedCacheManager(cacheManager);
+            Map<String, RedisCacheConfiguration> configMap = Arrays.stream(CacheConfigEnums.values())
+                    .collect(Collectors.toMap(CacheConfigEnums::getCacheName,
+                            t -> RedisCacheConfiguration.defaultCacheConfig()
+                                    .entryTtl(t.getTtl())
+                                    .prefixCacheNameWith(CacheConfigEnums.COMMON_CACHE_KEY)
+                                    .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
+                                    .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))));
+            builder.cacheDefaults(config)
+                    .withInitialCacheConfigurations(configMap)
+                    .transactionAware();
+        };
     }
 }
